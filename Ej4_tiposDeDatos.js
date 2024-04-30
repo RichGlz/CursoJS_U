cdc4c53c8fcb96f72dd688b529c04a5afa3799bf
Ricardo Gonzalez <ricardo.gonzaleza@yucatan.gob.mx>
2024-04-30 16:54:02 -0600

    Avances 30_04_24

---
+++ Ej4_tiposDeDatos.js
@@ -0,0 +1,10 @@
+"Number"
+    10,20,3.9,-54.2
+"string"
+    "hola", 'dos', `mundos` // (backtick, acento al revés)
+"Boolean"
+    true; false 
+"null"
+    null        // Ausencia de la referencia de un objeto
+"undefined"
+    undefined   // Ausencia de valor
---
+++ ej3_NotacionDeVariables.js
@@ -0,0 +1,8 @@
+let $miNumero1 = 10;
+console.log($miNumero1);
+
+let _miNumero2 = 20;
+console.log(_miNumero2);
+
+let miNumero3 = 30;
+console.log(miNumero3);
